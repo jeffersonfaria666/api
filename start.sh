f4479f6174af68c2e1cc98e6ddb9d4341cc0016c
jeffersonfaria666 <heresyeti@gmail.com>
2025-12-05 21:43:52 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e
 
-# Crear directorio temporal si no existe
-mkdir -p /tmp/downloads
-
-# Instalar dependencias si es necesario
-# (Render ya ejecuta pip install -r requirements.txt en build)
-
-# Iniciar la aplicación
-exec gunicorn --bind 0.0.0.0:$PORT server:app --workers 2 --threads 4 --timeout 300
